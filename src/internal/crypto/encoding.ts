25b17207e5a8c5c260f179218256e2c982df7b1f
Adrien Pinet <apinet@users.noreply.github.com>
2023-11-26 16:16:34 +0100

    add decodingbase64

--- src/internal/crypto/encoding.ts
+++ src/internal/crypto/encoding.ts
@@ -14,3 +14,14 @@ export const arrayTo64 = function (bytes: Uint8Array) {
 
   return base64String.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
 };
+
+export const decodeFromSafe64 = function (base64: string) {
+  // Add removed at end '='
+  base64 += Array(5 - (base64.length % 4)).join("=");
+
+  base64 = base64
+    .replace(/\-/g, "+") // Convert '-' to '+'
+    .replace(/\_/g, "/"); // Convert '_' to '/'
+
+  return Buffer.from(base64, "base64");
+};
